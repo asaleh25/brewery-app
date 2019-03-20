26fd20e8d2bebbc3bbaea95353eb0cf5fddb6d8d
Abdullah Al-Saleh <asaleh25@gmail.com>
2019-03-20 17:12:37 -0400

    More fixes

--- src/Beers.js
+++ src/Beers.js
@@ -13,8 +13,9 @@ class Beers extends React.Component {
   }
 
   query(pageNum) {
+    let beerList = [];
     let url = `https://sandbox-api.brewerydb.com/v2/beers/?key=20b378f58ba545551afeaf7f29820c4c&p=${pageNum}`;
-    fetch(url)
+    return(fetch(url)
       .then(results => {
         return results.json();
       })
@@ -31,27 +32,41 @@ class Beers extends React.Component {
             }
           )
         })
-        this.setState({beers: beers});
+        beerList.push(beers)
+        //this.setState({beers: beers});
+        return beerList;
       })
+    )
   }
 
   componentDidMount() {
-    this.query(5)
+    let biggerList = [];
+    var promises = [];
+    for (var i = 1; i < 8; i++) {
+      setTimeout(promises.push(this.query(i)), 20000);
+    }
+    Promise.all(promises).then(propList => {
+      let list = propList.map((lists) => {
+        biggerList.push(lists);
+      })
+      var output = biggerList.flat();
+      this.setState({beers: output.flat()});
+    })
   }
 
   caret(direction) {
     if (direction === 'asc') {
       return (
-        <i class="fas fa-sort-up"></i>
+        <i className="fas fa-sort-up"></i>
       );
     }
     if (direction === 'desc') {
       return (
-        <i class="fas fa-sort-down"></i>
+        <i className="fas fa-sort-down"></i>
       );
     }
     return (
-      <i class="fas fa-sort"></i>
+      <i className="fas fa-sort"></i>
     )
   }
 
@@ -61,7 +76,10 @@ class Beers extends React.Component {
         <BootstrapTable className='table table-striped table-sortable' data={this.state.beers}
           search={true} strictSearch={false} options={this.options} version='4'
           pagination={true}>
-          <TableHeaderColumn isKey={true} dataField='beerName' dataSort={true}
+          <TableHeaderColumn isKey={true} dataField='key' hidden={true}>
+            Key
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField='beerName' dataSort={true}
             caretRender={this.caret} width='20%' tdStyle={{whiteSpace: 'normal'}}>
             Beer Name
           </TableHeaderColumn>
